9aae32835a0ad3cf8ec9d2dfa152898aaded613d
Shajid Hossain <sajidhossainhemal@gmail.com>
2020-07-12 20:51:52 +0600

    Fourth Commit: Spawing Endless Background

---
+++ Assets/Scripts/Background Scripts/BackgroundSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSpawner : MonoBehaviour
+{
+    private GameObject[] backgrounds;
+    private float lastY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetBackgroundsAndSetLastY();
+    }
+
+    private void GetBackgroundsAndSetLastY() {
+        backgrounds = GameObject.FindGameObjectsWithTag("Background");
+        lastY = backgrounds[0].transform.position.y;
+
+        for(int i = 1; i < backgrounds.Length; i++) {
+            if(lastY > backgrounds[i].transform.position.y) {
+                lastY = backgrounds[i].transform.position.y;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Background") {
+            if(other.transform.position.y == lastY) {
+                Vector3 temp = other.transform.position;
+                float height = ((BoxCollider2D)other).size.y;
+
+                for(int i = 0; i < backgrounds.Length; i++) {
+                    if(!backgrounds[i].activeInHierarchy) {
+                        temp.y -= height;
+                        lastY = temp.y;
+
+                        backgrounds[i].transform.position = temp;
+                        backgrounds[i].SetActive(true);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerBounds.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBounds : MonoBehaviour
+{
+    private float minX;
+    private float maxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetMinXAndMaxX();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = transform.position;
+        
+        if(transform.position.x < minX) {
+            temp.x = minX;
+            transform.position = temp;
+        } else if(transform.position.x > maxX) {
+            temp.x = maxX;
+            transform.position = temp;
+        }
+    }
+
+    private void SetMinXAndMaxX() {
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+
+        maxX = bounds.x - 0.5f;
+        minX = -bounds.x + 0.5f;
+    }
+}
