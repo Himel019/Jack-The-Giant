226e25e2db65d11ebaf824b53ed65e79f8e114a9
Shajid Hossain <sajidhossainhemal@gmail.com>
2020-07-14 21:01:40 +0600

    Sixth Commit: Highscore Menu & Options Menu Setup

---
+++ Assets/Scripts/Game Controller/MainMenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("4_GameScene");
+    }
+
+    public void GoToHighScoreMenu() {
+        SceneManager.LoadScene("2_HighScore");
+    }
+
+    public void GoToOptionsMenu() {
+        SceneManager.LoadScene("3_Options");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void MusicButton() {
+        
+    }
+}
---
+++ Assets/Scripts/Game Controller/OptionsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void GoToMainMenu() {
+        SceneManager.LoadScene("1_MainMenu");
+    }
+}
